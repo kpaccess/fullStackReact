f91e8afb59a1da8f0844974beb488fd5880fe4f6
krishnapradhan <kpaccess@gmail.com>
2020-10-29 10:46:44 -0400

    added proxy in package json to handle CORS changes. Connected with backend database to get data

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import PageNotFound from "./PageNotFound";
@@ -6,6 +6,19 @@ import PageNotFound from "./PageNotFound";
 const ArticlePage = ({ match }) => {
     const { name } = match.params;
     const article = articleContent.find(article => article.name === name);
+
+
+    const [ articleInfo, setArticleInfo ] = useState({ upvotes: 0, comments: []});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
+
     if(!article) {
         return <PageNotFound />
     }
@@ -15,6 +28,7 @@ const ArticlePage = ({ match }) => {
     return (
         <div>
             <h1>{article.title}</h1>
+            <p>This post has been upvotes {articleInfo.upvotes} times</p>
             {
                 article.content.map((paragraph, key) =>
                     <p key={key}>{paragraph}</p>
